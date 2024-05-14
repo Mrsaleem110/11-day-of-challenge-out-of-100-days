c6f0bb0d172fe22c418d3c4a5789596f17d785c2
muhammad_saleem <160356353+Mrsaleem110@users.noreply.github.com>
2024-05-14 11:53:29 +0500

    adding 11 day of challenge

---
+++ Question 31/main.js
@@ -0,0 +1,10 @@
+// *Question 31:* No Users: Ensure your user list isn’t empty.
+// *Explain & TIP:* Before greeting users, check if there are any. This teaches you to handle empty arrays.
+var usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Greet users
+}
+// Removing all usernames ensures the message "We need to find some users!" is printed.
